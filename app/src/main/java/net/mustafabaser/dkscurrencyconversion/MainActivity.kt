0f6993aeb6ea65091f6ad870051a2b1983afabe9
MustafaBaser <32432625+MustafaBaser@users.noreply.github.com>
2020-09-11 19:01:57 +0300

    v1.4

--- app/src/main/java/net/mustafabaser/dkscurrencyconversion/MainActivity.kt
+++ app/src/main/java/net/mustafabaser/dkscurrencyconversion/MainActivity.kt
@@ -6,6 +6,8 @@ import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import android.widget.AdapterView.OnItemSelectedListener
@@ -26,6 +28,9 @@ class MainActivity : AppCompatActivity() {
     var convertedToCurrency = "USD"
     var conversionRate = 0f
 
+    //val firstCurrency = spinner.selectedItem.toString()
+    //val secondCurrency = spinner2.selectedItem.toString()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -34,13 +39,32 @@ class MainActivity : AppCompatActivity() {
         textChangedStuff()
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if(id==R.id.descriptionButton){
+            val intent = Intent(this, DescriptionActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
     private fun textChangedStuff() {
         et_firstConversion.addTextChangedListener(object : TextWatcher {
             override fun afterTextChanged(s: Editable?) {
                 try {
                     getApiResult()
                 } catch (e: Exception) {
-                    Toast.makeText(applicationContext, getString(R.string.typeAValue), Toast.LENGTH_SHORT).show()
+                    Toast.makeText(
+                        applicationContext,
+                        getString(R.string.typeAValue),
+                        Toast.LENGTH_SHORT
+                    ).show()
                 }
             }
 
@@ -57,13 +81,13 @@ class MainActivity : AppCompatActivity() {
     private fun getApiResult() {
         if (et_firstConversion != null && et_firstConversion.text.isNotEmpty() && et_firstConversion.text.isNotBlank()) {
             var API =
-                    "https://api.ratesapi.io/api/latest?base=$baseCurrency&symbols=$convertedToCurrency"
+                "https://api.ratesapi.io/api/latest?base=$baseCurrency&symbols=$convertedToCurrency"
 
             if (baseCurrency == convertedToCurrency) {
                 Toast.makeText(
-                        applicationContext,
-                        getString(R.string.PickACurrencyToConvert),
-                        Toast.LENGTH_SHORT
+                    applicationContext,
+                    getString(R.string.PickACurrencyToConvert),
+                    Toast.LENGTH_SHORT
                 ).show()
             } else {
                 GlobalScope.launch(Dispatchers.IO) {
@@ -72,16 +96,16 @@ class MainActivity : AppCompatActivity() {
                         val apiResult = URL(API).readText()
                         val jsonObject = JSONObject(apiResult)
                         conversionRate =
-                                jsonObject.getJSONObject("rates").getString(convertedToCurrency)
-                                        .toFloat()
+                            jsonObject.getJSONObject("rates").getString(convertedToCurrency)
+                                .toFloat()
 
                         Log.d("Main", "$conversionRate")
                         Log.d("Main", apiResult)
 
                         withContext(Dispatchers.Main) {
                             val text =
-                                    ((et_firstConversion.text.toString()
-                                            .toFloat()) * conversionRate).toString()
+                                ((et_firstConversion.text.toString()
+                                    .toFloat()) * conversionRate).toString()
                             et_secondConversion?.setText(text)
                         }
 
@@ -94,13 +118,14 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun spinnerSetup() {
+
         val spinner: Spinner = findViewById(R.id.spinner_firstConversion)
         val spinner2: Spinner = findViewById(R.id.spinner_secondConversion)
 
         ArrayAdapter.createFromResource(
-                this,
-                R.array.currencies,
-                android.R.layout.simple_spinner_item
+            this,
+            R.array.currencies,
+            android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
@@ -108,9 +133,9 @@ class MainActivity : AppCompatActivity() {
         }
 
         ArrayAdapter.createFromResource(
-                this,
-                R.array.currencies2,
-                android.R.layout.simple_spinner_item
+            this,
+            R.array.currencies2,
+            android.R.layout.simple_spinner_item
         ).also { adapter ->
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
@@ -122,10 +147,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
             ) {
                 baseCurrency = parent?.getItemAtPosition(position).toString()
                 getApiResult()
@@ -137,10 +162,10 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
             ) {
                 convertedToCurrency = parent?.getItemAtPosition(position).toString()
                 getApiResult()
@@ -149,7 +174,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun intentDescription(view: View) {
-        val intent = Intent(this, DescriptionActivity::class.java)
-        startActivity(intent)
+
     }
 }
