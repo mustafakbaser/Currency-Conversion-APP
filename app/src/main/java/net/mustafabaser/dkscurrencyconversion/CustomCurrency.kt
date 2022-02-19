7d88fb6c6f4a03af5a7627bf7bad61a743528916
Mustafa <mkursatbaser@gmail.com>
2022-02-19 12:56:31 +0300

    Custom Currency Logic

--- app/src/main/java/net/mustafabaser/dkscurrencyconversion/CustomCurrency.kt
+++ app/src/main/java/net/mustafabaser/dkscurrencyconversion/CustomCurrency.kt
@@ -1,15 +1,119 @@
 package net.mustafabaser.dkscurrencyconversion
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.json.JSONObject
+import java.net.URL
 
 var baseCurrency = "EUR"
-//var targetCurrency = "USD"
+var targetCurrency = "USD"
 var conversionRate = 0f
+var amount = 1
 
 class CustomCurrency : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_currency)
+        spinnerSetup()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val id = item.itemId
+        if (id == R.id.descriptionButton) {
+            val intent = Intent(this, DescriptionActivity::class.java)
+            startActivity(intent)
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun getApiResult() {
+            //API Document: https://www.frankfurter.app/docs/
+            var API =
+                "https://api.frankfurter.app/latest?amount=1&from=$baseCurrency&to=TRY"
+                GlobalScope.launch(Dispatchers.IO) {
+                    try {
+                        val apiResult = URL(API).readText()
+                        val jsonObject = JSONObject(apiResult)
+                        conversionRate =
+                            jsonObject.getJSONObject("rates").getString("TRY")
+                                .toFloat()
+
+                        Log.d("Main", "$conversionRate")
+                        Log.d("Main", apiResult)
+
+                        withContext(Dispatchers.Main) {
+                            val text =
+                                ((amount.toFloat()) * conversionRate).toString()
+                            euroTextView.setText(text)
+                        }
+
+                    } catch (e: Exception) {
+                        Log.e("Main", "$e")
+                    }
+                }
+            }
+
+    private fun spinnerSetup() {
+
+        val spinner: Spinner = findViewById(R.id.targetCurrency)
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currencies,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.currencies2,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        }
+
+        spinner.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                baseCurrency = parent?.getItemAtPosition(position).toString()
+                getApiResult()
+            }
+        })
+    }
+
+    fun toastMessage(view: View) {
+        Toast.makeText(this, getString(R.string.resultCalculation), Toast.LENGTH_LONG)
+            .show()
     }
 }
