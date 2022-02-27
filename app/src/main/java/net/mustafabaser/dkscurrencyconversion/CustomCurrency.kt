206456cf98e09d986a6d45850665b66fd190bb3a
mustafabaser <mkursatbaser@gmail.com>
2022-02-27 11:39:23 +0300

    Custom Currency Logic

--- app/src/main/java/net/mustafabaser/dkscurrencyconversion/CustomCurrency.kt
+++ app/src/main/java/net/mustafabaser/dkscurrencyconversion/CustomCurrency.kt
@@ -9,10 +9,8 @@ import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.view.View
-import android.widget.AdapterView
-import android.widget.ArrayAdapter
-import android.widget.Spinner
-import android.widget.Toast
+import android.widget.*
+import kotlinx.android.synthetic.main.activity_custom_currency.*
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.GlobalScope
@@ -25,6 +23,9 @@ var baseCurrency = "EUR"
 var targetCurrency = "USD"
 var conversionRate = 0f
 var amount = 1
+
+var API =
+    "https://api.frankfurter.app/latest?amount=1&from=EUR&to=$baseCurrency"
 
 class CustomCurrency : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -47,33 +48,6 @@ class CustomCurrency : AppCompatActivity() {
         }
         return super.onOptionsItemSelected(item)
     }
-
-    private fun getApiResult() {
-            //API Document: https://www.frankfurter.app/docs/
-            var API =
-                "https://api.frankfurter.app/latest?amount=1&from=$baseCurrency&to=TRY"
-                GlobalScope.launch(Dispatchers.IO) {
-                    try {
-                        val apiResult = URL(API).readText()
-                        val jsonObject = JSONObject(apiResult)
-                        conversionRate =
-                            jsonObject.getJSONObject("rates").getString("TRY")
-                                .toFloat()
-
-                        Log.d("Main", "$conversionRate")
-                        Log.d("Main", apiResult)
-
-                        withContext(Dispatchers.Main) {
-                            val text =
-                                ((amount.toFloat()) * conversionRate).toString()
-                            euroTextView.setText(text)
-                        }
-
-                    } catch (e: Exception) {
-                        Log.e("Main", "$e")
-                    }
-                }
-            }
 
     private fun spinnerSetup() {
 
@@ -110,6 +84,143 @@ class CustomCurrency : AppCompatActivity() {
                 getApiResult()
             }
         })
+    }
+
+    private fun getApiResult() {
+        //API Document: https://www.frankfurter.app/docs/
+        //TO-DO
+        var fromCurrency = "EUR"
+        if(baseCurrency != fromCurrency) {
+            var API =
+                "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+        }
+        //baseCurrency ile hedef aynı olduğunda repo patlıyor.
+        when(baseCurrency){
+            "EUR" -> {
+                var fromCurrency = "EUR"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+                GlobalScope.launch(Dispatchers.IO) {
+                    try {
+                        val apiResult = URL(API).readText()
+                        val jsonObject = JSONObject(apiResult)
+                        conversionRate =
+                            jsonObject.getJSONObject("rates").getString(baseCurrency)
+                                .toFloat()
+
+                        val usdApiResult = URL(API).readText()
+                        val usdJsonObject = JSONObject(usdApiResult)
+                        var usdConversionRate =
+                            usdJsonObject.getJSONObject("rates").getString("USD")
+                                .toFloat()
+
+                        Log.d("Main", "$conversionRate")
+                        Log.d("Main", apiResult)
+
+                        withContext(Dispatchers.Main) {
+                            val text =
+                                ((amount.toFloat()) * conversionRate).toString()
+                            euroToCurrency.setText(text)
+
+                            val usdText =
+                                ((amount.toFloat()) * usdConversionRate).toString()
+                            usdToCurrency.setText(usdText)
+
+                            val tryText =
+                                ((amount.toFloat()) * conversionRate).toString()
+                            tryToCurrency.setText(tryText)
+
+                            val gbpText =
+                                ((amount.toFloat()) * conversionRate).toString()
+                            tryToCurrency.setText(tryText)
+                        }
+
+                    } catch (e: Exception) {
+                        Log.e("Main", "$e")
+                    }
+                }
+            }
+
+            "USD" -> {
+                var fromCurrency = "USD"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "TRY" -> {
+                var fromCurrency = "TRY"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "GBP" -> {
+                var fromCurrency = "GBP"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "JPY" -> {
+                var fromCurrency = "JPY"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "CHF" -> {
+                var fromCurrency = "CHF"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "CNY" -> {
+                var fromCurrency = "CNY"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "DKK" -> {
+                var fromCurrency = "DKK"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "AUD" -> {
+                var fromCurrency = "AUD"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+            "CAD" -> {
+                var fromCurrency = "CAD"
+                var API =
+                    "https://api.frankfurter.app/latest?amount=1&from=$fromCurrency&to=$baseCurrency"
+            }
+        }
+
+        GlobalScope.launch(Dispatchers.IO) {
+            try {
+                val apiResult = URL(API).readText()
+                val jsonObject = JSONObject(apiResult)
+                conversionRate =
+                    jsonObject.getJSONObject("rates").getString(baseCurrency)
+                        .toFloat()
+
+                Log.d("Main", "$conversionRate")
+                Log.d("Main", apiResult)
+
+                withContext(Dispatchers.Main) {
+                    val text =
+                        ((amount.toFloat()) * conversionRate).toString()
+                    euroToCurrency.setText(text)
+
+                    val usdText =
+                        ((amount.toFloat()) * conversionRate).toString()
+                    usdToCurrency.setText(usdText)
+
+                    val tryText =
+                        ((amount.toFloat()) * conversionRate).toString()
+                    tryToCurrency.setText(tryText)
+
+                    val gbpText =
+                        ((amount.toFloat()) * conversionRate).toString()
+                    tryToCurrency.setText(tryText)
+                }
+
+            } catch (e: Exception) {
+                Log.e("Main", "$e")
+            }
+        }
     }
 
     fun toastMessage(view: View) {
